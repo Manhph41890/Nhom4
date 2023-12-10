df9315030cd8bea5c818099fcb751aad1911a4e1
Vỹ <138800403+Hungvy24@users.noreply.github.com>
2023-12-10 23:39:01 +0700

    Update adddm.php

--- admin/danhmuc/adddm.php
+++ admin/danhmuc/adddm.php
@@ -1,1 +1,23 @@
+  <header>
+        <h1>Thêm danh mục mới</h1>
+    </header>
 
+    <section>
+        <form action="index.php?act=adddm" method="post">
+        <label for="ma-loai">Mã loại:</label>
+        <input type="text" id="ma-loai" name="maloai">
+
+        <label for="ten-loai">Tên loại:</label>
+        <input type="text" id="tenloai" name="tenloai">
+
+        <div class="actions"><br>
+            <input type="submit" name="themmoi" value="Thêm mới"><br><br>
+            <input type="reset" value="Nhập lại" ><br><br>
+            <a href="index.php?act=listdm">Danh sách</a>
+        </div>
+        <?php 
+            if(isset($thongbao)&&($thongbao!=""))
+            echo $thongbao;
+        ?>   
+        </form>
+    </section>
