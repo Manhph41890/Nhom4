311c8eff0ddfdeec4374682cbb78b0fcf1e8fedd
Vỹ <138800403+Hungvy24@users.noreply.github.com>
2023-12-13 00:06:51 +0700

    Update role.php

--- model/role.php
+++ model/role.php
@@ -4,6 +4,7 @@
         $listrole=pdo_query($sql);
         return $listrole;
     }
+
     function loadone_role($id) {
         $sql = "select * from role WHERE id = $id";
         $role = pdo_query_one($sql);
