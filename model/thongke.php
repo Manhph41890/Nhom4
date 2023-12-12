fc38ff45823dfa97131fa8bf2aff2438590094b9
Vỹ <138800403+Hungvy24@users.noreply.github.com>
2023-12-13 00:07:22 +0700

    Update thongke.php

--- model/thongke.php
+++ model/thongke.php
@@ -1,4 +1,5 @@
 <?php 
+
       function loadall_thongke(){
         $sql = "select danh_muc.id as madm, danh_muc.name_dm as tendm, count(hang_hoa.id) as countsp, min(hang_hoa.price) as minprice, max(hang_hoa.price) as maxprice, avg(hang_hoa.price) as avgprice from hang_hoa left join danh_muc on danh_muc.id = hang_hoa.iddm group by danh_muc.id order by danh_muc.id desc";
         $listtk=pdo_query($sql);
